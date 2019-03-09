ec7af942aa8498674c609363f229c414e3bb1484
Christopher Bradford <chbradford@csumb.edu>
2019-03-09 23:10:41 +0000

    Completed lab 5

---
+++ Labs/lab6/api/getProducts.php
@@ -0,0 +1,22 @@
+<?php
+$host = "localhost";
+$dbname = "ottermart";
+$username = "root";
+$password = "";
+//establish connection
+$dbConn = newPDO("mysql:host=$host;dbname=$dbname", $username, $password);
+//setting Errorhandling to exception
+$dbConn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+$sql = "SELECT * FROM om_product ORDER BY productPrice LIMIT 10";
+$stmt = $connection -> prepare($sql);  //$connection MUST be previously initialized
+$stmt->execute();
+$records = $stmt->fetchAll(PDO::FETCH_ASSOC); //use fetch for one record, fetchAll for multiple
+
+//print_r($records); //display array info (for testing purposes)
+//echo json_encode($records);
+
+echo $records[0]['productName'];
+
+
+?>
