fda6e1e149995ae24cffbb08b8c3c6dd9a067585
Christopher Bradford <chbradford@csumb.edu>
2019-04-24 00:00:02 +0000

    f

--- Hw/hw4/api/mapsProxy.php
+++ Hw/hw4/api/mapsProxy.php
@@ -1,15 +1,11 @@
 <?php
 
-//"https://api.darksky.net/forecast/e09f12ef88e89732b0624d3ea607867b/$long,$lat"
-
-$apikey = getenv('API_KEY');
-
 $lat = $_GET['lat'];
 $long = $_GET['long'];
 
 $curl = curl_init();
       curl_setopt_array($curl, array(
-      CURLOPT_URL => "https://api.darksky.net/forecast/$apikey/$long,$lat",
+      CURLOPT_URL => "https://api.darksky.net/forecast/e09f12ef88e89732b0624d3ea607867b/$long,$lat",
       CURLOPT_RETURNTRANSFER => true,
       CURLOPT_TIMEOUT => 30,
       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
